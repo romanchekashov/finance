97d392f7415dacf9c3eaf6085bdcd509914ecebb
romanchekashov <romanrich89@gmail.com>
2016-08-30 13:04:04 +0300

    first commit

---
+++ src/main/java/ru/besttuts/finance/Application.java
@@ -0,0 +1,24 @@
+package ru.besttuts.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * @author romanchekashov
+ * @since 02.06.2016
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
